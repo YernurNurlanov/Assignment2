5fd9608f60c9af068f90a30d10a87cbb94a50a16
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-15 18:12:44 +0600

    Test add(Object item, int index) method of MyArrayList

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,6 @@ public class Main {
         System.out.println(list.size());
         System.out.println(list.contains(1));
         System.out.println(list.contains("a"));
+        list.add(100, 4);
     }
 }
