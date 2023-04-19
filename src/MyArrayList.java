86814c03c8ad561063184548a6ba0f3f0b240d66
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-19 11:19:17 +0600

    Implement sort() method

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -90,5 +90,22 @@ public class MyArrayList<T> implements MyList<T>{
     }
     @Override
     public void sort() {
+        int n = arr.length;
+        try {
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                    if ((int)arr[j] > (int)arr[j + 1]) {
+                        // swap arr[j] and arr[j+1]
+                        T temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        catch (UnsupportedOperationException e){
+            System.out.println("Array has element which is not integer");
+        }
+
     }
 }
