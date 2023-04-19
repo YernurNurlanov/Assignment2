6f85725fef32fba78455cd8295daa04a4b5963e3
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-04-19 21:27:30 +0600

    test for MyLinkedList

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,26 @@ public class Main {
         System.out.println(list.indexOf("A"));
         list.clear();
         System.out.println(list.size());
+        
+        MyLinkedList<Object> list2 = new MyLinkedList<>();
+        list2.add(5);
+        list2.add(12);
+        list2.add("A");
+        list2.add(true);
+        list2.add(1);
+        list2.add("A");
+        list2.add(false);
+        System.out.println(list2.size());
+        System.out.println(list2.contains(12));
+        System.out.println(list2.contains("a"));
+        list2.add(111, 1);
+        System.out.println(list2.get(1));
+        list2.remove(3);
+        System.out.println(list2.get(3));
+        System.out.println(list2.remove(false));
+        System.out.println(list2.indexOf("A"));
+        System.out.println(list2.lastIndexOf("A"));
+        list2.clear();
+        System.out.println(list2.size());
     }
 }
